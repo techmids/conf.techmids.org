92ea62fa7e15c6bfa404bbd2b3d9b6e0287bbfce
Dr Tom Goodman <TauOmicronMuRS@Gmail.com>
2025-09-19 11:00:30 +0100

    Create flyer.jsx
    Doing this from my phone without testing it...

---
+++ src/pages/flyer.jsx
@@ -0,0 +1,11 @@
+import { useEffect } from "react"
+import { useRouter } from "next/router"
+
+export default function FlyersRedirect() {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.replace("/")
+  }, [router])
+
+  return null
