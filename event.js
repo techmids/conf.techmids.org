1dbb3132048f2ec99d33b3e17ab28fd1ba48e398
Ryan Lee <rol@bluetel.co.uk>
2026-04-22 08:08:27 +0100

    feat(10) Disable old pages and schedule as part of prep for 2026

--- event.js
+++ event.js
@@ -1,18 +1,19 @@
 import { OVERALL_END, OVERALL_START} from "schedule"
 
 export const EVENT = {
-    date: 'Friday 14th November 2025',
+    date: 'Coming Soon for 2026',
+    dateTbc: true,
     title: 'TechMids Conf',
     venue: 'Everyman Cinema',
     address: 'Mailbox, Birmingham',
     startTime: OVERALL_START,
     endTime: OVERALL_END,
     ticketLink: 'https://ti.to/tech-events-birmingham/techmids-conf-2025',
-    onSale: true, //swap to true to embed ticket widget
+    onSale: false, //swap to true to embed ticket widget
     soldOut: false,
     titoId: 'tech-events-birmingham/techmids-conf-2025',
     CFPLink: "https://www.papercall.io/techmids-conf-2025",
-    CFPOpen: true,
+    CFPOpen: false,
     speakersTBC: true,
     capacity: 250,
     tagline: "",
@@ -20,51 +21,51 @@ export const EVENT = {
 
 //  Marks what information is available on the website
 export const AVAILABLE_INFORMATION = {
-    scheduleAvailable: true,
-    locationAvailable: true, 
-    speakersAvailable: true,
-    sponsorsAvailable: true,
+    scheduleAvailable: false,
+    locationAvailable: false, 
+    speakersAvailable: false,
+    sponsorsAvailable: false,
 }
 
 export const sponsorTiers = {
-    headline: {
-        name: "Headline Sponsor",
-        sponsors: [
-            {
-                name: "Amazon Web Services",
-                image: "/sponsors/AWS.svg",
-                url: "https://aws.amazon.com/"
-            }
-        ]
-    },
-    silver: {
-        name: "Silver Sponsors",
-        sponsors: [
-            {
-                name: "AdvancedOne",
-                image: "/sponsors/OneAdvanced.png",
-                url: "https://www.oneadvanced.com/"
-            },
-            {
-                name: "BCS Birmingham Branch",
-                image: "/sponsors/bcs.jpg",
-                url: "https://www.bcs.org/membership-and-registrations/member-communities/birmingham-branch/"
-            }
-        ]
-    },
-    bronze: {
-        name: "Bronze Sponsors",
-        sponsors: [
-            {
-                name: "Bluetel",
-                image: "/sponsors/Bluetel.png",
-                url: "https://www.bluetel.co.uk/"
-            },
-            {
-                name: "Spinks",
-                image: "/sponsors/Spinks.png",
-                url: "https://www.wearespinks.com/"
-            }
-        ]
-    },
+//    headline: {
+//        name: "Headline Sponsor",
+//        sponsors: [
+//            {
+//                name: "Amazon Web Services",
+//                image: "/sponsors/AWS.svg",
+//                url: "https://aws.amazon.com/"
+//            }
+//        ]
+//    },
+//    silver: {
+//        name: "Silver Sponsors",
+//        sponsors: [
+//            {
+//                name: "AdvancedOne",
+//                image: "/sponsors/OneAdvanced.png",
+//                url: "https://www.oneadvanced.com/"
+//            },
+//            {
+//                name: "BCS Birmingham Branch",
+//                image: "/sponsors/bcs.jpg",
+//                url: "https://www.bcs.org/membership-and-registrations/member-communities/birmingham-branch/"
+//            }
+//        ]
+//    },
+//    bronze: {
+//        name: "Bronze Sponsors",
+//        sponsors: [
+//            {
+//                name: "Bluetel",
+//                image: "/sponsors/Bluetel.png",
+//                url: "https://www.bluetel.co.uk/"
+//            },
+//            {
+//                name: "Spinks",
+//                image: "/sponsors/Spinks.png",
+//                url: "https://www.wearespinks.com/"
+//            }
+//        ]
+//    },
 }
